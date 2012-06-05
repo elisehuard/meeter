5420a1c72aff61d015556fc7ee3572a49461a74a
Elise Huard <elise@jabberwocky.eu>
2012-06-05 11:36:06 +0100

    initial commit

---
+++ src/meeter/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns meeter.views.welcome
+  (:require [meeter.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to meeter"]))
